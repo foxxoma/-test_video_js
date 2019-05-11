8a5f1c785c1ea3d6052419d3a50b74f60a1a9ffe
Popka <foxxoma@gmail.com>
2019-05-11 15:59:45 +0300

    8

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-if(navigator.webkitGetUserMedia!=null) { 
-  var options = { 
-    video:true, 
-    audio:true 
-  }; 
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-  window.URL.createObjectURL = window.URL.createObjectURL || window.URL.webkitCreateObjectURL || window.URL.mozCreateObjectURL || window.URL.msCreateObjectURL;
-  // запрашиваем доступ к веб-камере
-  navigator.webkitGetUserMedia(options, 
-    function(stream) { 
-      // получаем тег video
-      var video = document.querySelector('video'); 
-      // включаем поток в магический URL
-      video.src = window.webkitURL.createObjectURL(stream); 
-    }, 
-    function(e) { 
-     alert("error happened"); 
-    } 
-  ); 
-}
+
+navigator.getUserMedia = (navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia);
+window.URL.createObjectURL = (window.URL.createObjectURL ||
+                          window.URL.webkitCreateObjectURL ||
+                          window.URL.mozCreateObjectURL ||
+                          window.URL.msCreateObjectURL);
+  
+ 
+navigator.getUserMedia({ video : true },   
+//---
+function ()
+{
+  var video = document.getElementById('video');
+  video.src = window.URL.createObjectURL(stream);
+}, 
+function()
+{
+  alert("No access to the camera!")
+});
