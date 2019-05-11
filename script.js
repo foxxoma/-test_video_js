00f3469bc9dfd4f00cb7c1803a02d2eb0a22522f
Popka <foxxoma@gmail.com>
2019-05-11 13:47:48 +0300

    3

--- script.js
+++ script.js
@@ -6,31 +6,28 @@ const P = document.getElementById('p');
 canv.width = window.innerWidth;
 canv.height = window.innerHeight;
 
-const video = document.getElementById("video"),
-localmediaStream =null,
-onCameraFail = function(e) {
-	 console.log('Camera did not work.', e); // Исключение на случай, если камера не работает
-     
-};
 
 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-window.URL = window.URL || window.webkitURL;
-navigator.getUserMedia({video: true}, function(stream) {
-            video.src = window.URL.createObjectURL(stream);
-            localMediaStream = stream;
-        }, onCameraFail);
 
-cameraInterval = setInterval(function(){ snapshot();}, 1);
-function snapshot(){
-       if(localMediaStream){
-              ctx.drawImage(video, 0, 0);
-        }
+if(navigator.webkitGetUserMedia!=null) { 
+  var options = { 
+    video:true, 
+    audio:true 
+  }; 
+  
+  // запрашиваем доступ к веб-камере
+  navigator.webkitGetUserMedia(options, 
+    function(stream) { 
+      // получаем тег video
+      var video = document.querySelector('video'); 
+      // включаем поток в магический URL
+      video.src = window.webkitURL.createObjectURL(stream); 
+    }, 
+    function(e) { 
+      console.log("error happened"); 
+    } 
+  ); 
 }
-
-
-
-
 
 
   		if ('ondeviceorientationabsolute' in window) { 
