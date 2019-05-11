ab3439b3fe9faa34b5d5062c4ecc13952b702bc8
Popka <foxxoma@gmail.com>
2019-05-11 14:19:45 +0300

    4

--- script.js
+++ script.js
@@ -1,54 +1,45 @@
+window.onload = function () {
+    var canvas = document.getElementById('canvas');
+    var video = document.getElementById('video');
+    var button = document.getElementById('button');
+    var allow = document.getElementById('allow');
+    var context = canvas.getContext('2d');
+    var videoStreamUrl = false;
 
-//const P = document.getElementById('p');
+    // функция которая будет выполнена при нажатии на кнопку захвата кадра
+    var captureMe = function () {
+      if (!videoStreamUrl) alert('То-ли вы не нажали "разрешить" в верху окна, то-ли что-то не так с вашим видео стримом')
+      // переворачиваем canvas зеркально по горизонтали (см. описание внизу статьи)
+      context.translate(canvas.width, 0);
+      context.scale(-1, 1);
+      // отрисовываем на канвасе текущий кадр видео
+      context.drawImage(video, 0, 0, video.width, video.height);
+      // получаем data: url изображения c canvas
+      var base64dataUrl = canvas.toDataURL('image/png');
+      context.setTransform(1, 0, 0, 1, 0, 0); // убираем все кастомные трансформации canvas
+      // на этом этапе можно спокойно отправить  base64dataUrl на сервер и сохранить его там как файл (ну или типа того) 
+      // но мы добавим эти тестовые снимки в наш пример:
+      var img = new Image();
+      img.src = base64dataUrl;
+      window.document.body.appendChild(img);
+    }
 
-//canv.width = window.innerWidth;
-//canv.height = window.innerHeight;
+    button.addEventListener('click', captureMe);
 
+    // navigator.getUserMedia  и   window.URL.createObjectURL (смутные времена браузерных противоречий 2012)
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    window.URL.createObjectURL = window.URL.createObjectURL || window.URL.webkitCreateObjectURL || window.URL.mozCreateObjectURL || window.URL.msCreateObjectURL;
 
-
-
-if(navigator.webkitGetUserMedia!=null) { 
-  var options = { 
-    video:true, 
-    audio:true 
-  }; 
-  
-  // запрашиваем доступ к веб-камере
-  navigator.webkitGetUserMedia(options, 
-    function(stream) { 
-      // получаем тег video
-      var video = document.querySelector('video'); 
-      // включаем поток в магический URL
-      video.src = window.webkitURL.createObjectURL(stream); 
-    }, 
-    function(e) { 
-      console.log("error happened"); 
-    } 
-  ); 
-}
-
-/*
-  		if ('ondeviceorientationabsolute' in window) { 
-
-		
-		window.ondeviceorientationabsolute = function(event) { 
-
-		P.textContent = event.alpha;
-		} 
-	}
-		else if ('ondeviceorientation' in window) { 
-			
-			window.ondeviceorientationabsolute = function(event) { 
-
-		P.textContent = event.alpha;
-
-		}
-	}
-
-		else{
-		P.textContent = "f2";
-		}
-    
-
-
-*/
+    // запрашиваем разрешение на доступ к поточному видео камеры
+    navigator.getUserMedia({video: true}, function (stream) {
+      // разрешение от пользователя получено
+      // скрываем подсказку
+      allow.style.display = "none";
+      // получаем url поточного видео
+      videoStreamUrl = window.URL.createObjectURL(stream);
+      // устанавливаем как источник для video 
+      video.src = videoStreamUrl;
+    }, function () {
+      console.log('что-то не так с видеостримом или пользователь запретил его использовать :P');
+    });
+  };
