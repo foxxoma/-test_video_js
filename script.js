df1edbe5135968171cb943e7ede1ed4e4b939425
Yokio <foxxoma@gamail.com>
2019-05-14 15:53:19 +0400

    10

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 var video = document.getElementById('video');
-
+var front = false;
 // Get access to the camera!
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+	var myConstraints = {  video: { facingMode: (front? "user" : "environment")} }; 
     // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+    navigator.mediaDevices.getUserMedia(myConstraints).then(function(stream) {
         //video.src = window.URL.createObjectURL(stream);
         video.srcObject = stream;
         video.play();
