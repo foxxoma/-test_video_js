550313e8cd15642b78df0c0e4c15090f10ddee9b
Popka <foxxoma@gmail.com>
2019-07-17 17:11:42 +0300

    хех

--- script.js
+++ script.js
@@ -36,25 +36,25 @@ navigator.geolocation.getCurrentPosition(function(position) {
 });
 
 function throttle(callback, wait, immediate = false) {
-  let timeout = null 
-  let initialCall = true
-  
-  return function() {
-    const callNow = immediate && initialCall
-    const next = () => {
-      callback.apply(this, arguments)
-      timeout = null
-    }
-    
-    if (callNow) { 
-      initialCall = false
-      next()
-    }
+	let timeout = null
+	let initialCall = true
 
-    if (!timeout) {
-      timeout = setTimeout(next, wait)
-    }
-  }
+	return function() {
+		const callNow = immediate && initialCall
+		const next = () => {
+			callback.apply(this, arguments)
+			timeout = null
+		}
+
+		if (callNow) {
+			initialCall = false
+			next()
+		}
+
+		if (!timeout) {
+			timeout = setTimeout(next, wait)
+		}
+	}
 }
 
 // Get access to the camera!
